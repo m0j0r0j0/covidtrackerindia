86587494073949b99c41984d6731b3f74b5561d9
m0j0r0j0 <prasadtikkas2@gmail.com>
2021-05-16 21:33:46 +0530

    swagger getmapping added

--- src/main/java/com/prad/controller/TrackerController.java
+++ src/main/java/com/prad/controller/TrackerController.java
@@ -32,6 +32,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
@@ -306,6 +308,16 @@ public class TrackerController {
 	
 	private static String format(String pattern, Object value) {
 	    return new DecimalFormat(pattern).format(value);
+	}
+	
+	@PostMapping("/testswagger")
+	@ResponseBody
+	public State getSwagger(@RequestBody District s2) {
+		State s = new State();
+		s.setId(s2.getId());
+		s.setState("Test");
+		
+		return s;
 	}
 	
 }
