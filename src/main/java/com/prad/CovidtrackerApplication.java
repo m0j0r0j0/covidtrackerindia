4fbd6def85374476949b1ba0625e87e59d0b65ae
m0j0r0j0 <prasadtikkas2@gmail.com>
2021-05-16 21:13:41 +0530

    Added Swagger

--- src/main/java/com/prad/CovidtrackerApplication.java
+++ src/main/java/com/prad/CovidtrackerApplication.java
@@ -1,18 +1,14 @@
 package com.prad;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
-import com.prad.dto.VaccineCenterDto;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CovidtrackerApplication {
 
 	public static void main(String[] args) {
